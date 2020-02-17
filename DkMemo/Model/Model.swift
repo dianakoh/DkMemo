4234fa5dbccda64704bdf5cce854235133576957
dianakoh <dianakoh94@gmail.com>
2020-02-18 01:34:24 +0900

    메모리스트 모델 구현

---
+++ DkMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  DkMemo
+//
+//  Created by Gayoung on 2020/02/18.
+//  Copyright © 2020 Diana Koh. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + 👍 = 🌙")
+    ]
+}
