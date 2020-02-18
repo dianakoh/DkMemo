5f308bddc77437eda239bc93b66b0266e7182ff6
dianakoh <dianakoh94@gmail.com>
2020-02-18 13:35:24 +0900

    new memo screen

---
+++ DkMemo/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  DkMemo
+//
+//  Created by Gayoung on 2020/02/18.
+//  Copyright © 2020 Diana Koh. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
