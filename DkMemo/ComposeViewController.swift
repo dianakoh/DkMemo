8b66cba134a78989776220a8d753beb6da570acd
dianakoh <dianakoh94@gmail.com>
2020-02-18 16:49:52 +0900

    DataManager

--- DkMemo/ComposeViewController.swift
+++ DkMemo/ComposeViewController.swift
@@ -24,8 +24,9 @@ class ComposeViewController: UIViewController {
             return
         }
         
-        let newMemo = Memo(content: memo)
-        Memo.dummyMemoList.append(newMemo)
+//        let newMemo = Memo(content: memo)
+//        Memo.dummyMemoList.append(newMemo)
+        
         NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
         dismiss(animated: true, completion: nil)
     }
---
+++ DkMemo/Data/DataManager.swift
@@ -0,0 +1,65 @@
+//
+//  DataManager.swift
+//  DkMemo
+//
+//  Created by Gayoung on 2020/02/18.
+//  Copyright © 2020 Diana Koh. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataManager {
+    static let shared = DataManager()
+    private init() {
+        
+    }
+    
+    var mainContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    var memoList = [Memo]()
+    
+    func fetchMemo() {
+        let request: NSFetchRequest<Memo> = Memo.fetchRequest()
+        
+        let sortByDateDesc = NSSortDescriptor(key: "insertDate", ascending: false)
+        request.sortDescriptors = [sortByDateDesc]
+        
+        do {
+            memoList = try mainContext.fetch(request)
+        } catch {
+            print(error)
+        }
+            
+    }
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "DkMemo")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
